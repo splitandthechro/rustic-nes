4c11130ad88cd419656f475ad9eaa7ac4423bb44
splittydev <splittydev@protonmail.com>
2018-06-09 12:45:50 +0200

    Add memory write8 and lots of memory tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate arrayref;
 
-
 mod cartridge;
 mod memory;
 
@@ -10,6 +9,6 @@ use memory::*;
 
 fn main() {
     let cart_file = ::std::fs::read("nestest.nes").expect("Unable to read cartridge!");
-    let _cart = Cart::new(cart_file);
-    let _mem = Memory::new();
+    let cart = Cart::new(cart_file);
+    let _mem = Memory::new(cart.header.mapper());
 }
--- src/memory/memory.rs
+++ src/memory/memory.rs
@@ -1,11 +1,31 @@
 pub struct Memory {
     pub bytes: [u8; 0x800],
+    mapper: u8,
 }
 
 impl Memory {
-    pub fn new() -> Self {
+    pub fn new(mapper: u8) -> Self {
         Memory {
             bytes: [0u8; 0x800],
+            mapper: mapper,
+        }
+    }
+
+    pub fn read8(&self, addr: u16) -> u8 {
+        match addr {
+            0x0000...0x2000 => self.bytes[addr as usize % 0x800],
+            0x8000...0xFFFF if self.mapper == 0 => 0, // prg_rom[addr as usize - 0x8000]
+            0x8000...0xFFFF if self.mapper == 1 => 0, // prg_rom[addr as usize]
+            0x8000...0xFFFF => panic!("Unimplemented mapper: {}", self.mapper),
+            _ => panic!("Invalid address: 0x{:X}", addr),
+        }
+    }
+
+    pub fn write8(&mut self, addr: u16, val: u8) {
+        match addr {
+            0x0000...0x2000 => self.bytes[addr as usize % 0x800] = val,
+            0x4017 => (), // Ignore write to PPU or CPU register
+            _ => panic!("Cannot write at address: 0x{:X}", addr),
         }
     }
 }
@@ -16,6 +36,34 @@ mod tests {
 
     #[test]
     fn initializes_correctly() {
-        Memory::new();
+        Memory::new(0);
+    }
+
+    #[test]
+    fn reads_ram_without_error() {
+        let mem = Memory::new(0);
+        (0_u16..0x2000_u16).for_each(|i| {
+            let b = mem.read8(i);
+            assert_eq!(0_u8, b);
+        });
+    }
+
+    #[test]
+    fn writes_ram_without_error() {
+        let mut mem = Memory::new(0);
+        (0_u16..0x2000_u16).for_each(|i| {
+            let wb = (i % 255) as u8;
+            mem.write8(i, wb);
+            let rb = mem.read8(i);
+            assert_eq!(wb, rb);
+        });
+    }
+
+    #[test]
+    fn wraps_correctly() {
+        let mut mem = Memory::new(0);
+        (0x000_u16..0x0800_u16).for_each(|i| assert_eq!(0_u8, mem.read8(i)));
+        (0x800_u16..0x1000_u16).for_each(|i| mem.write8(i, 1_u8));
+        (0x000_u16..0x0800_u16).for_each(|i| assert_eq!(1_u8, mem.read8(i)));
     }
 }
